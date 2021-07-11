dfd5d93cce42dc199691031a0ec32a9a7b3eede7
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-11 16:42:14 -0300

    Adding handleCreateNewTask implementation to create a new task.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,18 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) {
+      window.alert("POr favor, informar um título para sua tarefa");
+      return
+    }
+
+    const newTask = {
+      id: tasks.length+1,
+      title:newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks(tasks.concat(newTask));
   }
 
   function handleToggleTaskCompletion(id: number) {
